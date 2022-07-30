6a17ff43fcb573f041e8a6aa26ef46a46ab9072c
Sergei Zimmerman <serjtsimmerman@gmail.com>
2022-07-31 02:54:41 +0300

    Add test for select rank

--- hwt/lib/test/test_private.cc
+++ hwt/lib/test/test_private.cc
@@ -1,7 +1,7 @@
 #include <cstddef>
+#include <cstdlib>
 #include <functional>
 #include <gtest/gtest.h>
-#include <cstdlib>
 
 #define private public
 #define protected public
@@ -34,9 +34,7 @@ std::pair<std::size_t, bool> validate_red_black_helper(base_node_ptr p_base) {
 }
 
 bool validate_size_helper(base_node_ptr p_base) {
-  if (!p_base) {
-    return {true};
-  }
+  if (!p_base) { return {true}; }
 
   if (base_node::size(p_base) != base_node::size(p_base->m_left_) + base_node::size(p_base->m_right_) + 1) {
     return false;
@@ -46,31 +44,27 @@ bool validate_size_helper(base_node_ptr p_base) {
   bool valid_right = validate_size_helper(p_base->m_right_);
 
   return valid_left && valid_right;
-} 
+}
 } // namespace
 
-TEST(test_rb_tree_private, test_1) { 
-  rb_tree_ranged_<int, std::less<int>> t; 
+TEST(test_rb_tree_private, test_1) {
+  rb_tree_ranged_<int, std::less<int>> t;
   bool thrown = false;
-  
+
   try {
     for (int i = 0; i < 131072; i++) {
-    t.insert(i);
+      t.insert(i);
     }
-  } catch (...) {
-    thrown = true;
-  }
+  } catch (...) { thrown = true; }
 
   EXPECT_EQ(validate_red_black_helper(t.m_root_).second && !thrown, true);
   thrown = false;
 
   try {
     for (int i = 0; i < 32768; i++) {
-    t.erase(i);
+      t.erase(i);
     }
-  } catch (...) {
-    thrown = true;
-  }
+  } catch (...) { thrown = true; }
 
   EXPECT_EQ(validate_red_black_helper(t.m_root_).second && !thrown, true);
 }
@@ -78,19 +72,42 @@ TEST(test_rb_tree_private, test_1) {
 TEST(test_rb_tree_private, test_2) {
   rb_tree_ranged_<int, std::less<int>> t;
   bool thrown = false;
-  
+
   try {
     for (int i = 0; i < 65536; i++) {
       int temp = std::rand();
-      if (!t.contains(temp)) {
-        t.insert(temp);
-      }
+      if (!t.contains(temp)) { t.insert(temp); }
     }
-  } catch (...) {
-    thrown = true;
-  }
+  } catch (...) { thrown = true; }
 
   EXPECT_EQ(validate_size_helper(t.m_root_) && !thrown, true);
+}
+
+TEST(test_rb_tree_private, test_3) {
+  rb_tree_ranged_<int, std::less<int>> t;
+  for (int i = 0; i < 1024; i++) {
+    t.insert(i);
+  }
+
+  for (int i = 0; i < 1024; i++) {
+    ASSERT_TRUE(t.contains(i));
+  }
+
+  t.erase(666);
+  ASSERT_FALSE(t.contains(666));
+}
+
+TEST(test_rb_tree_private, test_4) {
+  rb_tree_ranged_<int, std::less<int>> t;
+  for (int i = 1; i < 131072; i++) {
+    t.insert(i);
+  }
+
+  ASSERT_EQ(t.select_rank(1), 1);
+
+  for (int i = 1; i < 131072; i++) {
+    ASSERT_EQ(t.select_rank(i), i);
+  }
 }
 
 int main(int argc, char *argv[]) {
