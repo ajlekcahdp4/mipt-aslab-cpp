8c2a461266289e18005f50a3bac03031fc96d432
Sergei Zimmerman <serjtsimmerman@gmail.com>
2022-08-01 00:23:21 +0300

    README

--- hwt/test/queries/src/queries.cc
+++ hwt/test/queries/src/queries.cc
@@ -1,30 +1,10 @@
 #include <chrono>
 #include <iostream>
 
-#ifdef BOOST_FOUND__
-#include <boost/program_options.hpp>
-#include <boost/program_options/option.hpp>
-namespace po = boost::program_options;
-#endif
-
 #include <order_statistic_set.hpp>
 
 int main(int argc, char *argv[]) {
   if (!std::cin || !std::cout) { std::abort(); }
-
-#ifdef BOOST_FOUND__
-  po::options_description desc("Available options");
-  desc.add_options()("help,h", "Print this help message");
-
-  po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm);
-
-  if (vm.count("help")) {
-    std::cout << desc << "\n";
-    return 1;
-  }
-#endif
   throttle::order_statistic_set<int> t{};
 
   while (true) {
