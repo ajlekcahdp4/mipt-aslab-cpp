6dbaefa5fc9d2bd58afb68a59cb2f904eb8944fe
ajlekcahdp4 <alex.rom23@mail.ru>
2022-09-11 19:27:11 +0300

    tried to add test_2d_segment_segment, needs to be overwritten, but tests were passed

--- 02-geometry/01-hw3d/lib/include/primitives/segment2.hpp
+++ 02-geometry/01-hw3d/lib/include/primitives/segment2.hpp
@@ -10,6 +10,7 @@
 
 #pragma once
 
+#include "equal.hpp"
 #include "point2.hpp"
 #include "vec2.hpp"
 
@@ -33,9 +34,46 @@ template <typename T> struct segment2 {
 
   bool contains(const point_type &point) const {
     vec_type segment_vec = b - a, vec = b - point;
-    return (co_directional(segment_vec, vec) && segment_vec.length_sq() > vec.length_sq());
+    return (co_directional(segment_vec, vec) && is_roughly_greater_eq(segment_vec.length_sq(), vec.length_sq()));
   }
 };
+
+template <typename T> bool operator==(const segment2<T> &lhs, const segment2<T> &rhs) {
+  return (lhs.a == rhs.a && lhs.b == rhs.b);
+}
+
+template <typename T> bool test_2d_segment_segment(const segment2<T> &seg1, const segment2<T> &seg2) {
+  auto r = seg1.b - seg1.a;
+  auto s = seg2.b - seg2.a;
+
+  if (seg1 == seg2) return true;
+
+  auto p = seg1.a - point2<T>::origin();
+  auto q = seg2.a - point2<T>::origin();
+
+  auto dir_cross = perp_dot(r, s);
+  auto diff_start_cross = perp_dot(q - p, r);
+
+  if (dir_cross == 0) {
+    if (diff_start_cross == 0) {
+      auto t0 = dot(q - p, r) / r.length_sq();
+      auto t1 = t0 + dot(s, r) / r.length_sq();
+
+      if ((is_roughly_greater_eq(t0, 0.0f) && is_roughly_less_eq(t0, 1.0f)) ||
+          (is_roughly_greater_eq(t1, 0.0f) && is_roughly_less_eq(t1, 1.0f)))
+        return true;
+    }
+    return false;
+  }
+
+  auto t = perp_dot(q - p, s) / perp_dot(r, s);
+  auto u = perp_dot(q - p, r) / perp_dot(r, s);
+  if ((is_roughly_greater_eq(t, 0.0f) && is_roughly_less_eq(t, 1.0f)) &&
+      (is_roughly_greater_eq(u, 0.0f) && is_roughly_less_eq(u, 1.0f)))
+    return true;
+
+  return false;
+}
 
 } // namespace geometry
 } // namespace throttle
--- 02-geometry/01-hw3d/lib/test/test_segment2.cc
+++ 02-geometry/01-hw3d/lib/test/test_segment2.cc
@@ -35,3 +35,69 @@ TEST(test_segment2, test_contain_point) {
   EXPECT_TRUE(a.contains(p1));
   EXPECT_FALSE(a.contains(p2));
 }
+
+TEST(test_segment2, test_segment_segment_1) {
+  segment_type a({0, 0}, {1, 1});
+  segment_type b({0, 1}, {1, 0});
+  segment_type c({0, 1}, {0, 2});
+
+  EXPECT_TRUE(test_2d_segment_segment(a, b));
+  EXPECT_FALSE(test_2d_segment_segment(a, c));
+}
+
+TEST(test_segment2, test_segment_segment_2) {
+  segment_type a({0, 0}, {1, 1});
+  segment_type b({0.5, 0.5}, {2, 2});
+
+  EXPECT_TRUE(test_2d_segment_segment(a, b));
+}
+
+TEST(test_segment2, test_segment_segment_3) {
+  segment_type a({0, 0}, {1, 1});
+  segment_type b({0.5, 0.5}, {0.5, 0.5});
+
+  EXPECT_TRUE(test_2d_segment_segment(a, b));
+}
+
+TEST(test_segment2, test_segment_segment_4) {
+  segment_type a({1, 1}, {1, 1});
+  segment_type b({1, 1}, {1, 1});
+
+  EXPECT_TRUE(test_2d_segment_segment(a, b));
+}
+
+TEST(test_segment2, test_segment_segment_5) {
+  segment_type a({0, 0}, {0, 1});
+  segment_type b({1, 0}, {1, 1});
+
+  EXPECT_FALSE(test_2d_segment_segment(a, b));
+}
+
+TEST(test_segment2, test_segment_segment_6) {
+  segment_type a({0, 0}, {0, 1});
+  segment_type b({0, 1}, {0, 2});
+
+  EXPECT_TRUE(test_2d_segment_segment(a, b));
+}
+
+TEST(test_segment2, test_segment_segment_7) {
+  segment_type a({0, 0}, {0, 1});
+  segment_type b({0, 2}, {0, 3});
+  std::cout << a.signed_distance(b.a) << " " << a.signed_distance(b.b);
+
+  EXPECT_FALSE(test_2d_segment_segment(a, b));
+}
+
+TEST(test_segment2, test_segment_segment_8) {
+  segment_type a({0, 0}, {1, 0});
+  segment_type b({1, 2}, {2, -3});
+
+  EXPECT_FALSE(test_2d_segment_segment(a, b));
+}
+
+TEST(test_segment2, test_segment_segment_9) {
+  segment_type a({0, 0}, {0, 0});
+  segment_type b({2, 2}, {2, 2});
+
+  EXPECT_FALSE(test_2d_segment_segment(a, b));
+}
