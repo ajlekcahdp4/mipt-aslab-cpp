06f9e2c29415198044d31faa4e6a824b7f57de4a
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-09-19 11:03:50 +0300

    added first draft of the insert function in uniform grid

--- 02-geometry/01-hw3d/lib/include/broadphase/uniform_grid.hpp
+++ 02-geometry/01-hw3d/lib/include/broadphase/uniform_grid.hpp
@@ -14,6 +14,7 @@
 #include "equal.hpp"
 #include "narrowphase/collision_shape.hpp"
 #include "point3.hpp"
+#include "vec3.hpp"
 
 #include <list>
 #include <unordered_map>
@@ -27,14 +28,12 @@ template <typename T, typename t_shape = collision_shape<T>,
 class uniform_grid : broadphase_structure<uniform_grid<T>, t_shape> {
 
   struct cell {
-    point3<T> point_t m_minimum_corner;
+    point3<unsigned> point_t m_minimum_corner;
   };
-
-  using cell_type = cell<T>;
 
   struct cell_hash {
     // compute hash bucket index in range [0, NUM_BUCKETS-1]
-    int operator()(const cell_type &cell) {
+    int operator()(const cell &cell) {
       const int h1 = 0x8da6b343; // Large multiplication constants;
       const int h2 = 0xd8163841; // here arbirarily chosen primes
       const int h3 = 0xcb1ab31f;
@@ -65,6 +64,7 @@ class uniform_grid : broadphase_structure<uniform_grid<T>, t_shape> {
 
 public:
   using shape_type = t_shape;
+  using vector_type = vec3<int>;
 
   /* ctor with hint about the number of shapes to insert */
   uniform_grid(unsigned number_hint) {
@@ -93,13 +93,13 @@ public:
 
   std::vector<shape_ptr> many_to_many() {
     rebuild();
-    std::set<unsigned> in_collision;
 
-    for (auto &list : m_map) {
-      auto intersecting = find_all_collisions(list);
-      for (auto &elem : intersecting)
-        set.insert(elem);
-    }
+    auto in_collision = find_all_collisions();
+
+    std::vector<shape_ptr> result;
+    std::transform(in_collision.begin(), in_collision.end(), std::back_inserter(result),
+                   [&](const auto &elem) { return std::addressof(m_stored_shapes[elem]); });
+    return result;
   }
 
   void rebuild() {
@@ -116,6 +116,8 @@ public:
       m_stored_shapes.push_back(idx);
       insert(idx);
     }
+
+    m_waiting_queue.clear();
   }
 
 private:
@@ -123,10 +125,39 @@ private:
    * find all cells the shape overlaps and insert shape's index into
    * the corresponding lists.
    */
-  void insert(unsigned) {}
+  void insert(unsigned idx) {
+    std::set<vector_type> cells{};
+    cells.reserve(8);
 
-  /* returns the set off all intersecting shapes in the cell's list */
-  std::set<unsigned> find_all_collisions(shape_list_t) const {}
+    bool  straddling = false;
+    auto &shape = m_stored_shapes[idx];
+
+    auto          &bbox = shape.bounding_box();
+    auto           min_corner = vector_type(bbox.minimum_corner());
+    std::vector<T> hwidths = {2 * bbox.m_halfwidth_x, 2 * bbox.m_halfwidth_y, 2 * bbox.m_halfwidth_x};
+
+    cells.insert(min_corner / m_cell_size);
+
+    cells.insert((min_corner + vector_type{hwidths[0], 0, 0}) / m_cell_size);
+    cells.insert((min_corner + vector_type{0, hwidths[1], 0}) / m_cell_size);
+    cells.insert((min_corner + vector_type{0, 0, hwidths[2]}) / m_cell_size);
+
+    cells.insert((min_corner + vector_type{0, hwidths[1], hwidths[2]}) / m_cell_size);
+    cells.insert((min_corner + vector_type{hwidths[0], 0, hwidths[2]}) / m_cell_size);
+    cells.insert((min_corner + vector_type{hwidths[0], hwidths[1], 0}) / m_cell_size);
+
+    cells.insert((min_corner + vector_type{hwidths[0], hwidths[1], hwidths[2]}) / m_cell_size);
+
+    for (auto &cell : cells)
+      m_map[cell].push_back(idx);
+  }
+
+}
+
+/* returns the set off all intersecting shapes in the cell's list */
+std::set<unsigned>
+find_all_collisions(shape_list_t cell_list) const {}
+
 };
 
 } // namespace geometry
