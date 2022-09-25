d2b8a74f887e67485acb43e44077937ec6863561
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-09-25 09:17:03 +0300

    renamed aabb to axis_aligned_bb

--- 02-geometry/01-hw3d/lib/include/narrowphase/aabb.hpp
+++ 02-geometry/01-hw3d/lib/include/narrowphase/aabb.hpp
@@ -20,7 +20,7 @@
 namespace throttle {
 namespace geometry {
 
-template <typename T> struct aabb {
+template <typename T> struct axis_aligned_bb {
   using vec_type = vec3<T>;
   using point_type = point3<T>;
 
@@ -29,20 +29,20 @@ template <typename T> struct aabb {
   T          m_halfwidth_y;
   T          m_halfwidth_z;
 
-  aabb(const point_type &p_center, T half_x, T half_y, T half_z)
+  axis_aligned_bb(const point_type &p_center, T half_x, T half_y, T half_z)
       : m_center{p_center}, m_halfwidth_x{half_x}, m_halfwidth_y{half_y}, m_halfwidth_z{half_z} {}
 
-  aabb(const point_type &p_center, T halfwidth)
+  axis_aligned_bb(const point_type &p_center, T halfwidth)
       : m_center{p_center}, m_halfwidth_x{halfwidth}, m_halfwidth_y{halfwidth}, m_halfwidth_z{halfwidth} {}
 
-  aabb(const point_type &first, const point_type &second)
+  axis_aligned_bb(const point_type &first, const point_type &second)
       : m_center{first + T{0.5f} * (second - first)}, m_halfwidth_x{std::abs(first.x - second.x) / T{2.0f}},
         m_halfwidth_y{std::abs(first.y - second.y) / T{2.0f}}, m_halfwidth_z{std::abs(first.z - second.z) / T{2.0f}} {}
 
   template <typename... Ts, typename = std::enable_if_t<std::conjunction_v<std::is_convertible<Ts, point_type>...>>>
-  aabb(Ts... points)
-      : aabb(point_type{vmin(points.x...), vmin(points.y...), vmin(points.z...)},
-             point_type{vmax(points.x...), vmax(points.y...), vmax(points.z...)}) {}
+  axis_aligned_bb(Ts... points)
+      : axis_aligned_bb(point_type{vmin(points.x...), vmin(points.y...), vmin(points.z...)},
+                        point_type{vmax(points.x...), vmax(points.y...), vmax(points.z...)}) {}
 
   point_type minimum_corner() const {
     return {m_center.x - m_halfwidth_x, m_center.y - m_halfwidth_y, m_center.z - m_halfwidth_z};
@@ -55,7 +55,7 @@ template <typename T> struct aabb {
   // returns the maximum width in all 3 dimensions
   T max_width() const { return 2 * vmax(m_halfwidth_x, m_halfwidth_y, m_halfwidth_z); }
 
-  bool intersect(const aabb &a) const {
+  bool intersect(const axis_aligned_bb &a) const {
     if (is_definitely_greater(std::abs(m_center.x - a.m_center.x), (m_halfwidth_x + a.m_halfwidth_x))) return false;
     if (is_definitely_greater(std::abs(m_center.y - a.m_center.y), (m_halfwidth_y + a.m_halfwidth_y))) return false;
     if (is_definitely_greater(std::abs(m_center.z - a.m_center.z), (m_halfwidth_z + a.m_halfwidth_z))) return false;
